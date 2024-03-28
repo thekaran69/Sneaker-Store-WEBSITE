48448a4bee48a00501095b98173fb391f97bd6ef
Karan Singh <119948976+thekaran69@users.noreply.github.com>
2024-03-29 01:00:08 +0530

    adding click event by javascript

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const wrapper = document.querySelector(".sliderWrapper")
+const menuItem = document.querySelectorAll(".menuItem")
+
+menuItem.forEach((item,index)=>{
+    item.addEventListener("click", () => {
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
